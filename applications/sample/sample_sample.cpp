cd3b91817867eec68395ce190ea4b0613299e2f7
Alex Schwartz <schwartz_alex@pm.me>
2024-01-27 17:40:33 +0000

    * Almost compiled selene, need to remove TIFF from CMake build

--- applications/sample/sample_sample.cpp
+++ applications/sample/sample_sample.cpp
@@ -1,16 +1,16 @@
 #include <sample_sample.hpp>
 
-// #pragma GCC diagnostic push
-// #pragma GCC diagnostic ignored "-Wuseless-cast"
-// #pragma GCC diagnostic ignored "-Wold-style-cast"
-// #include <selene/img_io/IO.hpp>
-// #include <selene/img_io/jpeg/Read.hpp>
-// #pragma GCC diagnostic pop
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wuseless-cast"
+#pragma GCC diagnostic ignored "-Wold-style-cast"
+#include <selene/img_io/IO.hpp>
+#include <selene/img_io/jpeg/Read.hpp>
+#pragma GCC diagnostic pop
 
 namespace sample {
 
     Sample::Sample(){
-        // sln::DynImage img_data = sln::read_image(sln::FileReader("/home/aschwartz/Downloads/kitten.jpg"));
+        sln::DynImage img_data = sln::read_image(sln::FileReader("/home/aschwartz/Downloads/kitten.jpg"));
     }
 
 }
