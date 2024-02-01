84417cc0316d6513d6b12256895c2ff1c7b1f151
Alex Schwartz <schwartz_alex@pm.me>
2024-02-01 03:58:35 +0000

    added commented function as a template for primarily cudascii usage

--- libraries/cudascii/cudascii_dummy.cpp
+++ libraries/cudascii/cudascii_dummy.cpp
@@ -21,4 +21,35 @@ namespace cudascii {
 
     }
 
+    // KEEP THIS FOR LATER!!!
+    // std::tuple<int, int, int> image_to_ascii(const std::string &filename) {
+
+    //     // Load Image using CImg
+    //     cimg_library::CImg<unsigned char> src(filename);
+
+    //     // Get the image dimensions
+    //     int width = src.width();
+    //     int height = src.height();
+    //     int channels = src.channels();
+
+    //     // Assess how much memory is needed for image
+    //     const unsigned int N = width*height*channels;
+    //     const unsigned int bytes = N * sizeof(char);
+
+    //     // Allocate GPU memory
+    //     int *d_a;
+    //     cudaMalloc((char**)&d_a, bytes);
+
+    //     // Copy the image from host (CPU) to device (GPU)
+    //     cudaMemcpy(d_a, src, bytes, cudaMemcpyHostToDevice);
+
+    //     // Call the pixel_to_ascii code here
+
+    //     // Copy the ascii array from device (GPU) to host (CPU)
+    //     cudaMemcpy(h_a, d_a, bytes, cudaMemcpyDeviceToHost);
+
+    //     // Don't forget to free memory!!!!
+
+    // }
+    
 }
