6f46fcfdfd93a5e813034eba2f7633ff0c6d9c7e
alex <alex.h.groeger@gmail.com>
2024-03-21 02:57:47 +0000

    changed pixel_to_ascii function signature

--- libraries/cudascii/cudascii_dummy.cu
+++ libraries/cudascii/cudascii_dummy.cu
@@ -4,24 +4,24 @@
 #include <string>
 
 namespace {
-// Algorithm Parameterization
-// const std::string gray_levels_fine =
-// "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~i!lI;:,\"^`. ";
-const constexpr char* gray_level_lookup{"@%#*+=-:. "};
-const constexpr int gray_levels = 10;
-const constexpr float RED_WEIGHT = 0.2126;
-const constexpr float GREEN_WEIGHT = 0.7152;
-const constexpr float BLUE_WEIGHT = 0.0722;
-const constexpr float CONVERSION_THRESHOLD = 0.0031308;
-const constexpr float BELOW_THRESHOLD_SCALAR = 12.92;
-const constexpr float ABOVE_THRESHOLD_SCALAR = 1.055;
-const constexpr float ABOVE_THRESHOLD_EXPONENT = 1 / 2.4;
-const constexpr float ABOVE_THRESHOLD_OFFSET = -0.055;
-}  // namespace
+    // Algorithm Parameterization
+    // const std::string gray_levels_fine =
+    // "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~i!lI;:,\"^`. ";
+    const constexpr char* gray_level_lookup{"@%#*+=-:. "};
+    const constexpr int gray_levels = 10;
+    const constexpr float RED_WEIGHT = 0.2126;
+    const constexpr float GREEN_WEIGHT = 0.7152;
+    const constexpr float BLUE_WEIGHT = 0.0722;
+    const constexpr float CONVERSION_THRESHOLD = 0.0031308;
+    const constexpr float BELOW_THRESHOLD_SCALAR = 12.92;
+    const constexpr float ABOVE_THRESHOLD_SCALAR = 1.055;
+    const constexpr float ABOVE_THRESHOLD_EXPONENT = 1 / 2.4;
+    const constexpr float ABOVE_THRESHOLD_OFFSET = -0.055;
+}
 
 namespace cudascii {
 
-    __global__ void pixel_to_ascii(unsigned char *out, unsigned char *r, unsigned char *g, unsigned char *b) {
+    __global__ void pixel_to_ascii(unsigned char *out, unsigned char r, unsigned char g, unsigned char b) {
 
         // Thread index
         int i = threadIdx.x + blockIdx.x * blockDim.x;
@@ -30,7 +30,7 @@ namespace cudascii {
         int gray_index;
         
         // Standard linear combination
-        c_linear = RED_WEIGHT*(*r/255.) + GREEN_WEIGHT*(*g/255.) + BLUE_WEIGHT*(*b/255.);
+        c_linear = RED_WEIGHT*(r/255.) + GREEN_WEIGHT*(g/255.) + BLUE_WEIGHT*(b/255.);
         
         // If gray level is very dark, use linear scaling
         if (c_linear <= CONVERSION_THRESHOLD)
